581d48702cce62d8a7a0094f6732c2cd51eee8d5
RanyellHenrique <ranyellhenrique@gmail.com>
2019-08-01 16:47:27 -0300

    Projeto Criado

---
+++ src/entities/Funcionarios.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Funcionarios {
+	
+	private String name;
+	private String email;
+	private Double salario;
+	
+	
+	public Funcionarios(String name, String email, Double salario) {
+		this.name = name;
+		this.email = email;
+		this.salario = salario;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+		@Override
+	public String toString() {
+		return "Funcionarios [name=" + name + ", email=" + email + ", salario=" + salario + "]";
+	}
+
+}
